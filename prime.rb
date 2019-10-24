401c41ebdbd650f63c645ca281a84beff62ed554
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-10-24 21:13:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,9 @@
 def prime?(n)
   array=[]
    (2..n-1).to_a.each do |i|
-    if 
-      return true
-    else 
-      return false
+    if n % i==0
+       return false
     end
   end
+  return true
 end
